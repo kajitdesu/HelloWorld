9f579254afd7d8990a54fa236692a6a3a343ca92
Dmitriy <desukajit@gmail.com>
2022-08-05 23:04:03 +0600

    add if and else

---
+++ Example_If_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "kajit")
+{
+    Console.WriteLine("Ура, это же kajit!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
